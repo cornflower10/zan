534384711b2f969b17ae5800b1aafd8e4899898c
cornflower <861281331@qq.com>
2019-08-11 22:39:31 +0800

    Initial commit

---
+++ miniprogram/pages/zan/zan.js
@@ -0,0 +1,73 @@
+// miniprogram/pages/zan/zan.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userIconUrl:"",
+    usserName:"",
+    desc:"",
+    type:"1",//1单图，2多图，3链接
+    urls:[],
+    url:"",
+    zan: ['fjefj', 'dfhsk', 'dfhsk', 'dfhsk', 'dfhsk', 'dfhsk', 'dfhsk', 'dfhsk', 'dfhsk', 'dfhsk', 'dfhsk', 'dfhsk', 'dfhsk', 'dfhsk', 'dfhsk', 'dfhsk']
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
